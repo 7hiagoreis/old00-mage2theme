232810bce175320c8d6035c77a509e17c73f2c18
Thiago Reis <thiagoaurea@gmail.com>
2025-05-03 02:29:35 -0300

    Create Index.php

---
+++ app/code/DiskT/ThemeOptions/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,23 @@
+<?php
+namespace DiskT\ThemeOptions\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(
+        Action\Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
