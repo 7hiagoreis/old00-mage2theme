5858131b27d462ac9d466029c31bb80dd157f296
Thiago Reis <thiagoaurea@gmail.com>
2025-05-14 09:34:48 -0300

    Update Save.php

--- app/code/DiskT/ThemeOptions/Controller/Adminhtml/Index/Save.php
+++ app/code/DiskT/ThemeOptions/Controller/Adminhtml/Index/Save.php
@@ -1,23 +1,36 @@
 <?php
+
 namespace DiskT\ThemeOptions\Controller\Adminhtml\Index;
 
 use Magento\Backend\App\Action;
 use Magento\Framework\App\Config\Storage\WriterInterface;
 use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Cache\Frontend\Pool;
 
 class Save extends Action
 {
     protected $configWriter;
     protected $resultRedirectFactory;
+    protected $storeManager;
+    protected $cacheTypeList;
+    protected $cacheFrontendPool;
 
     public function __construct(
         Action\Context $context,
         WriterInterface $configWriter,
-        RedirectFactory $resultRedirectFactory
+        RedirectFactory $resultRedirectFactory,
+        StoreManagerInterface $storeManager,
+        TypeListInterface $cacheTypeList,
+        Pool $cacheFrontendPool
     ) {
         parent::__construct($context);
         $this->configWriter = $configWriter;
         $this->resultRedirectFactory = $resultRedirectFactory;
+        $this->storeManager = $storeManager;
+        $this->cacheTypeList = $cacheTypeList;
+        $this->cacheFrontendPool = $cacheFrontendPool;
     }
 
     public function execute()
@@ -27,52 +40,42 @@ class Save extends Action
 
         if (!empty($data)) {
             try {
-                // Verifica se o modo Hacker foi enviado
+                $storeId = $this->storeManager->getStore()->getId();
+                $theme = $this->getRequest()->getParam('theme') ?? 'default';
+
                 if (isset($data['modo_hacker'])) {
                     $modoHacker = $data['modo_hacker'];
 
                     if ($modoHacker == '1') {
-                        // Tema escuro (modo hacker)
-                        $this->configWriter->save('diskt_theme/general/background_color', '#000000', 'default', 0);
-                        $this->configWriter->save('diskt_theme/general/font_color', '#ffffff', 'default', 0);
-                        $this->configWriter->save('diskt_theme/general/link_color', '#00ff00', 'default', 0);
+                        $this->configWriter->save("diskt_theme/$theme/background_color", '#000000', 'stores', $storeId);
+                        $this->configWriter->save("diskt_theme/$theme/font_color", '#ffffff', 'stores', $storeId);
+                        $this->configWriter->save("diskt_theme/$theme/link_color", '#00ff00', 'stores', $storeId);
                     } elseif ($modoHacker == '0') {
-
-                        // Tema claro (modo clean)
-                        $this->configWriter->save('diskt_theme/general/background_color', '#ffffff', 'default', 0);
-                        $this->configWriter->save('diskt_theme/general/font_color', '#000000', 'default', 0);
-                        $this->configWriter->save('diskt_theme/general/link_color', '#0000ff', 'default', 0);
+                        $this->configWriter->save("diskt_theme/$theme/background_color", '#ffffff', 'stores', $storeId);
+                        $this->configWriter->save("diskt_theme/$theme/font_color", '#000000', 'stores', $storeId);
+                        $this->configWriter->save("diskt_theme/$theme/link_color", '#0000ff', 'stores', $storeId);
                     }
 
-                    // Sempre salva o valor do modo hacker
-                    $this->configWriter->save('diskt_theme/general/hacker_mode', $modoHacker, 'default', 0);
+                    $this->configWriter->save("diskt_theme/$theme/hacker_mode", $modoHacker, 'stores', $storeId);
                 }
 
-                // Sobrescreve com valores personalizados, se enviados
                 if (!empty($data['cor_principal'])) {
-                    $this->configWriter->save('diskt_theme/general/background_color', $data['cor_principal'], 'default', 0);
+                    $this->configWriter->save("diskt_theme/$theme/background_color", $data['cor_principal'], 'stores', $storeId);
                 }
 
                 if (!empty($data['cor_fonte'])) {
-                    $this->configWriter->save('diskt_theme/general/font_color', $data['cor_fonte'], 'default', 0);
+                    $this->configWriter->save("diskt_theme/$theme/font_color", $data['cor_fonte'], 'stores', $storeId);
                 }
 
                 if (!empty($data['cor_link'])) {
-                    $this->configWriter->save('diskt_theme/general/link_color', $data['cor_link'], 'default', 0);
-                }
-
-                if (!empty($data['cor_botao_primario'])) {
-                    $this->configWriter->save('diskt_theme/general/button_color', $data['cor_botao_primario'], 'default', 0);
-                }
-
-                if (!empty($data['cor_link_botao'])) {
-                    $this->configWriter->save('diskt_theme/general/link_button_color', $data['cor_link_botao'], 'default', 0);
+                    $this->configWriter->save("diskt_theme/$theme/link_color", $data['cor_link'], 'stores', $storeId);
                 }
 
                 if (!empty($data['font_family'])) {
-                    $this->configWriter->save('diskt_theme/general/font_family', $data['font_family'], 'default', 0);
+                    $this->configWriter->save("diskt_theme/$theme/font_family", $data['font_family'], 'stores', $storeId);
                 }
 
+                $this->flushCache();
                 $this->messageManager->addSuccessMessage(__('As configurações foram salvas com sucesso.'));
             } catch (\Exception $e) {
                 $this->messageManager->addErrorMessage(__('Erro ao salvar: ' . $e->getMessage()));
@@ -82,5 +85,17 @@ class Save extends Action
         }
 
         return $resultRedirect->setPath('adminhtml/system_config/edit/section/diskt_theme');
+    }
+
+    private function flushCache()
+    {
+        $cacheTypes = ['config', 'block_html', 'full_page'];
+        foreach ($cacheTypes as $cacheType) {
+            $this->cacheTypeList->cleanType($cacheType);
+        }
+
+        foreach ($this->cacheFrontendPool as $cacheFrontend) {
+            $cacheFrontend->getBackend()->clean();
+        }
     }
 }
